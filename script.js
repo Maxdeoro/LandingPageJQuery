987a4916e2c1570a3306eb278ee04e85995c66df
Maxdeoro <maxdeoro@gmail.com>
2024-11-17 23:15:43 +0300

    html layout

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#nav').localScroll(800);
+    $('#intro').parallax('50%', 0.1);   // 50%-horizontal position, 0.1-vertical scrolling speed
+    $('#second').parallax('50%', 0.1);
+    $('#bg').parallax('50%', 0.4);
+    $('#third').parallax('50%', 0.3);
+});
